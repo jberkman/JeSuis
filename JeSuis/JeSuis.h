b9939fc47d6506741c406419d3d16cad934e4325
jacob berkman <jacob@87k.net>
2015-10-12 18:43:22 -0700

    Initial Commit

---
+++ JeSuis/JeSuis.h
@@ -0,0 +1,19 @@
+//
+//  JeSuis.h
+//  JeSuis
+//
+//  Created by jacob berkman on 2015-10-12.
+//  Copyright © 2015 jacob berkman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JeSuis.
+FOUNDATION_EXPORT double JeSuisVersionNumber;
+
+//! Project version string for JeSuis.
+FOUNDATION_EXPORT const unsigned char JeSuisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JeSuis/PublicHeader.h>
+
+
